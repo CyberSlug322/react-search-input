bc9af57a72aead47bb21b553dc787b5d3567fa1b
Cyber Slug <lasminski@mail.ru>
2022-03-17 15:54:32 +0300

    feat: replace string with constant in SearchInput

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import ExtendedSearch from './ExtendedSearch';
-import { MODE_AFTER_ENTER_PRESSED, MODE_AFTER_STOP_TYPING, MODE_IMMEDIATE  } from './modes';
+import { searchMode  } from './modes';
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <ExtendedSearch mode = {MODE_AFTER_ENTER_PRESSED}/>
-        <ExtendedSearch mode = {MODE_AFTER_STOP_TYPING}/>
-        <ExtendedSearch mode = {MODE_IMMEDIATE}/>
+        <ExtendedSearch mode = {searchMode.MODE_AFTER_ENTER_PRESSED}/>
+        <ExtendedSearch mode = {searchMode.MODE_AFTER_STOP_TYPING}/>
+        <ExtendedSearch mode = {searchMode.MODE_IMMEDIATE}/>
       </header>
     </div>
   );
